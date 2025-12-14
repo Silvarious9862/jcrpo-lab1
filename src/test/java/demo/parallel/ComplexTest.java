eb49be0410c1de40de670395944fbb99f9f5bab7
Silvarious9862 <vbobrik0@gmail.com>
2025-12-14 11:44:23 +0300

    add ComplexTest

--- src/test/java/demo/parallel/ComplexTest.java
+++ src/test/java/demo/parallel/ComplexTest.java
@@ -1,6 +1,5 @@
-package test.demo.parallel;
+package demo.parallel;
 
-import demo.parallel.Complex;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -40,14 +39,6 @@ public class ComplexTest {
         Complex result = a.divide(b);
         assertEquals(-0.2, result.getRe(), 1e-9);
         assertEquals(0.4, result.getIm(), 1e-9);
-    }
-
-    @Test
-    void testConjugate() {
-        Complex a = new Complex(2, -3);
-        Complex result = a.conjugate();
-        assertEquals(2, result.getRe(), 1e-9);
-        assertEquals(3, result.getIm(), 1e-9);
     }
 
     @Test
